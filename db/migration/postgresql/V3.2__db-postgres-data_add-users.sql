c645c4a65a6c095aedb19c167b44488847fb0586
Willi Lazarov <lazarov.w@gmail.com>
2021-12-05 12:53:41 +0100

    Add model and migration

---
+++ db/migration/postgresql/V3.2__db-postgres-data_add-users.sql
@@ -0,0 +1,53 @@
+-- -----------------------------------------------------
+-- Data for table bds.auth_user
+-- -----------------------------------------------------
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('lazarov', 'lazarov@penterep.com', 'Willi', 'Lazarov', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', true, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('cpearl1', 'cpearl1@yandex.ru', 'Cris', 'Pearl', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('aryam2', 'aryam2@state.tx.us', 'Augusto', 'Ryam', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('sblowfield3', 'sblowfield3@unesco.org', 'Serge', 'Blowfield', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('mthurstance4', 'mthurstance4@ed.gov', 'Melody', 'Thurstance', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('aewins5','aewins5@cbslocal.com', 'Abra', 'Ewins', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('schatelet6', 'schatelet6@flavors.me', 'Stephanie', 'Chatelet', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('onickolls7', 'onickolls7@cocolog-nifty.com', 'Olympia', 'Nickolls', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('wgrob8', 'wgrob8@mediafire.com', 'Wilhelmina', 'Grob', 'pbkdf2_sha256$260000$qCMP5bpio8iLrFJvxnLykg$paEdvT8BsWOetmdEPT/1q+/3TzcfjIHjHH91SyW0VoE=', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('acrockatt9', 'acrockatt9@ycombinator.com', 'Ailis', 'Crockatt', '4d0ae2fb373131382febedaeb4337d5e69554e043a1289bbd3f154150ad19bd6', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('crippera', 'crippera@github.io', 'Cheston', 'Ripper', '0eb45cf87830f2b088264b94963a746322d26f7723309839f1b2a6936b672c1c', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('dklimusb', 'dklimusb@google.com.hk', 'Dotty', 'Klimus', '33b5ebbf55e14096505055c06de84432d43ab78a192ce945839f72a454a00fd1', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('bsavinec', 'bsavinec@techcrunch.com', 'Bonnee', 'Savine', 'b042a48ccc67c2aa93506b31294d3e2612962a76607c17f583aeaab3517acce2', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('igrocuttd', 'igrocuttd@typepad.com', 'Ileane', 'Grocutt', 'f17a3ba9dc73fb5b42116e1da33f85085b019ef404f4db727afef8727263ddf2', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('dolerenshawe', 'dolerenshawe@dedecms.com', 'Delphinia', 'Olerenshaw', '206f18bf5af0e351a7b0c1ca15457d57c8cd4913ef78942bd982f8d20cc97788', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('gsigsworthf', 'gsigsworthf@umn.edu', 'Gregoor', 'Sigsworth', '18aa9b13b21766d994412ecbe8bb26eb097ab036ddf17c645662a2fedbb8aed0', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('ebertholing', 'ebertholing@cbslocal.com', 'Ellette', 'Bertholin', 'de1f58f5246e85a87fc8c2e71f92308a13ddc16e6f785f2765b5a061098d7b8b', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('ffinlaterh', 'ffinlaterh@europa.eu', 'Farica', 'Finlater', '3bae90cebd4f023c9ce934d2fe9013526f2977a4984fb22831acce8f84b3df0b', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('jfatheri', 'jfatheri@dion.ne.jp', 'Jarrod', 'Father', '824fc8a57006154762876ad4e0f4949184133e54c6ca6fabce9d1fbc7797fd01', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('pmcmeekinj', 'pmcmeekinj@bing.com', 'Phillipe', 'Mc Meekin', '22643ae7e0956913ce8f57b99171011af4518ccae65f40e8fcaa70cc8011a143', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('kbernardosk', 'kbernardosk@shop-pro.jp', 'Kristoffer', 'Bernardos', 'dad4faeab6e8d555784d6df8ea8e0bc2cd69b884a0bf5370f300952fc7726e1c', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('dponderl', 'dponderl@shutterfly.com', 'Dill', 'Ponder', '09973755bfc563079e594027894fc95d02df0c61a95d6f9f2c61f4826b129ade', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('ctiptaftm', 'ctiptaftm@webmd.com', 'Connor', 'Tiptaft', '897aaa3594746b48e400342d6dc74bb1da2d99a433740dde2afbbb7491185092', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('nwhitseyn', 'nwhitseyn@theglobeandmail.com', 'Nap', 'Whitsey', '9949ae6a8ca22e0b7a408adec9eeae0f07721745479b775b2b51a9a9263e76b2', false, true, true, now());
+INSERT INTO bds.auth_user (username, email, first_name, last_name, password, is_superuser, is_staff, is_active, date_joined)
+  VALUES('sclausnero', 'sclausnero@globo.com', 'Shamus', 'Clausner', '546baf9a64e7ab510682f296183d25764b5543d68420bc450add159fde97b458', false, true, true, now());
